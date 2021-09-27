78b85bab88ecf44419c538cb5cddf2e59d4f7c91
lastorder <adzkiyadzikrullah@gmail.com>
2021-09-27 09:52:21 +0700

    Third Pull

--- app/src/main/java/yayasan/idn/myapplication/MainActivity.kt
+++ app/src/main/java/yayasan/idn/myapplication/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataEmail:List<String>
+    private lateinit var dataPassword:List<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,19 +19,29 @@ class MainActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.edPassword)
         val btnLogin = findViewById<Button>(R.id.btn_Login)
 
-        btnLogin.setOnClickListener{
-            if(username.text.isNotEmpty() || password.text.isNotEmpty()){
-                if(username.text.toString() == "1234" && password.text.toString() == "1234"){
-                    Toast.makeText(this,"Welcome",Toast.LENGTH_LONG).show()
-                    val intent = Intent(this, MoveActivity::class.java)
-                    startActivity(intent)
-                }else{
-                    Toast.makeText(this,"Username or Password Wrong!!",Toast.LENGTH_LONG).show()
-                }
+        dataEmail = listOf("adhi@gmail.com","joko@gmail.com","tono@gmail.com")
+        dataPassword = listOf("1234","12345")
+
+        btnLogin.setOnClickListener {
+            if(login(username.text.toString(),password.text.toString())){
+                Toast.makeText(this, "Logging", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this,MoveActivity::class.java)
+                startActivity(intent)
             }else{
-                Toast.makeText(this,"input required",Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"Username or Password Wrong",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+    private fun login(email:String, password:String):Boolean{
+        for(i in dataEmail.indices){
+            if(email == dataEmail[i] && password == dataPassword[i]){
+                return true
             }
         }
+        return false
     }
 
 
